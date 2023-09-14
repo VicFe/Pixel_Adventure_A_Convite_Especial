d0d47e74f5b96b13410bfd1d03bec150ea9a2d94
BrenoM <brenomartins.1747@gmail.com>
2023-09-14 20:57:15 -0300

    criação de icon para o jogo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,3 @@
 # CHANGELOG
 > 1.0    Breno-404      correção de erros é criação do `portal` para trocar de níveis
+> 1.0.1  Breno-404      criação de icon para o jogo
--- portal.gd
+++ portal.gd
@@ -1,18 +1,18 @@
 extends Area2D
 
-@export var room: PackedScene
-
 @onready var animPlayer: AnimationPlayer = $AnimationPlayer
 
+@export var room: PackedScene
+
 func _on_body_entered(_body: PhysicsBody2D):
+
 	teleport()
 		
-func _get_configuration_warning() -> String:
-	if (not room):
-		return "It is necessary to inform a room to change the scene"
-	return ""
-
 func teleport() -> void:
-	animPlayer.play("FADE_IN") #isso fara uma simples animação da tela fechando
-	await animPlayer.animation_finished
-	get_tree().change_scene_to_packed(room)
+	if room != null:  # Verifique se 'room' não é nulo
+		animPlayer.play("FADE_IN")
+		await animPlayer.animation_finished
+		get_tree().change_scene_to_packed(room)
+	else:
+		# Lida com o erro de 'room' sendo nulo
+		print("Erro: 'room' não está definido!")
